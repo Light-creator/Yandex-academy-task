94dc03c1858a2efe5ebc11d77bc82b861ee81bd3
Light-creator <ibadulasha@ya.ru>
2021-11-02 15:29:17 +0300

    Get handler birth stat

--- API/GET.php
+++ API/GET.php
@@ -15,24 +15,50 @@ class GET {
         if(count($url_arr) == 3 && $url_arr[0] == 'imports' && is_numeric($url_arr[1]) == true && $url_arr[2] == 'citizens') {
 
             $citizens = DB_PDO::$pdo->query("SELECT * FROM citizen WHERE import_id=".$url_arr[1])->fetchAll();
-            
+
             Response::response(200, $citizens);
 
         } else if(count($url_arr) == 4 && $url_arr[0] == 'imports' && is_numeric($url_arr[1]) == true && $url_arr[2] == 'citizens' && $url_arr[3] == 'birthdays') {
-
+            
             $citizens = DB_PDO::$pdo->query("SELECT * FROM citizen WHERE import_id=".$url_arr[1])->fetchAll();
-            $citizens_with_birth = self::filter_birth($citizens);
+            $list_month_presents = self::filter_birth($citizens);
 
+            print_r($list_month_presents);
         } else {
             Response::response(400, 'Bad url');
         }
         DB_PDO::close();
     }
 
-    private static function filter_birth($citizens, $month) {
-        foreach($citizens as $citizen) {
-            $citizen_month = explode('.', $citizen[]);
+    private static function filter_birth($citizens) {
+        $birth = [
+            'data' => []
+        ];
+        
+        for ($i=1; $i <= 12; $i++) { 
+            $birth['data'][$i] = [];
+            $list_prensents = [];
+
+            foreach($citizens as $citizen) {
+                $citizen_month = (int) explode('.', $citizen['birth_date'])[1];
+                if($i == $citizen_month) {
+                    $list_prensents = array_merge($list_prensents, json_decode($citizen['relatives']));
+                    unset($citizen);
+                }
+            }
+
+            $uniques = array_unique($list_prensents);
+            foreach($uniques as $citizen_id) {
+                $presents = count(array_keys($list_prensents, $citizen_id));
+
+                $birth['data'][$i][] = [
+                    'citizen_id' => $citizen_id,
+                    'presents' => $presents
+                ];
+            }
+
         }
+        return $birth;
     }
 
 }
